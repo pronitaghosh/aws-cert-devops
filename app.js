69cfe24b6fb8dd566c588e9ce97b682f0e6947e9
Pronita Ghosh <pronita.ghosh@gmail.com>
2025-05-02 19:49:35 -0700

    update with new text

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Hell to the world!!'));
+app.get('/', (req, res) => res.send('Hell to the Smart World in breaks!!'));
 
 app.listen(port);
 console.log(`Hell App running on http://localhost:${port}`);
